9cfc79607ea163dd5fd962a0f2a69633ecc37467
soulsam480 <soulsam480@hotmail.com>
2021-05-12 17:41:31 +0530

    chore: test deploy

---
+++ content/.vitepress/theme/components/About.vue
@@ -0,0 +1,346 @@
+<template>
+  <section>
+    <br />
+    <div narrow class="resume">
+      <div class="res-head">
+        <h1>Sambit Sahoo</h1>
+        <small class="muted">Full Stack Developer</small> <br />
+        <small class="muted"
+          ><a href="mailto:sambitsahoojs@gmail.com"
+            >sambitsahoojs@gmail.com</a
+          ></small
+        ><br />
+        <p>
+          <a
+            href="https://github.com/soulsam480"
+            target="_blank"
+            class="nav-link"
+          >
+            <small>github</small>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sambit-sahoo-6a3859190/"
+            target="_blank"
+            class="nav-link"
+          >
+            <small>likedIn</small>
+          </a>
+          <a
+            href="https://twitter.com/sambitsahoojs"
+            target="_blank"
+            class="nav-link"
+          >
+            <small>twitter</small>
+          </a>
+          <a
+            href="https://instagram.com/sambitsahoojs"
+            target="_blank"
+            class="nav-link"
+          >
+            <small>instagram</small>
+          </a>
+        </p>
+      </div>
+      <br />
+      <div class="res-container">
+        <p class="res-head-2"><span>About</span></p>
+        <div class="res-data">
+          <p>
+            Hello, I'm Sambit Sahoo, an undergraduate student and freelance
+            frontend developer. I'm passionate about coding as I love
+            materailizing an Idea to a smooth user interface.
+          </p>
+          <p>
+            Most of my work is open-source and I belive in the idea of community
+            built software. I feel that participating in open-source gives us an
+            opprtunity to gain more knowledge.
+          </p>
+          <p>
+            I'm also the Co-Founder/CTO of
+            <a href="https://tiaamo.com">Tiaamo.com</a>. We are a clothing
+            business which primarily operates in the wholesale domain. When I'm
+            not coding I mostly take photos or write articles. I'm an otaku at
+            heart and <em>One Piece </em> is my favourite anime.
+          </p>
+        </div>
+      </div>
+      <div class="res-container">
+        <p class="res-head-2"><span>Work Experience</span></p>
+
+        <div class="res-data row">
+          <div class="col-6">
+            <p>
+              <span>Tiaamo.com</span>
+              <small class="muted"><em>Co-Founder</em></small> <br />
+              <small class="muted">MARCH 2018 - PRESENT</small>
+              <a
+                href="https://noter.sambitsahoo.com"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>site</small>
+              </a>
+            </p>
+
+            <small>
+              Clothing Startup primarily operating in the wholesale domain.
+              Served over 50+ regular clients from various institutes and
+              Industries.
+            </small>
+          </div>
+          <div class="col-6">
+            <p>
+              <span>Public Incubator Inc.</span>
+              <span class="muted"><em>Frontend Intern</em></span>
+              <br />
+              <small class="muted">MARCH 2020 - APRIL 2020</small>
+              <a
+                href="https://www.publicincubator.com/"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>site</small>
+              </a>
+            </p>
+
+            <small>
+              Primarily worked on the Landing Page of the company.
+            </small>
+          </div>
+        </div>
+      </div>
+      <div class="res-container">
+        <p class="res-head-2"><span>Stack</span></p>
+        <div class="res-data">
+          <p>
+            <b>Frontend : </b> Typescript, Javascript, Vuejs, Reactjs, Electron
+          </p>
+          <p><b>Frontend Frameworks : </b> Quasar, Ionic</p>
+          <p><b>Backend : </b> Nodejs, PostgreSQL, Sqlite3, Serverless</p>
+          <p><b>DevOps : </b> Firebase</p>
+          <p><b>Languages : </b> Typescript, Javascript, Python</p>
+          <p><b>Graphics : </b> Adobe Photoshop, Adobe Illustrator</p>
+        </div>
+      </div>
+      <div class="res-container">
+        <p class="res-head-2"><span>Projects</span></p>
+        <div class="res-data row">
+          <div class="col-6">
+            <p>
+              <span>BillBird</span>
+              <a
+                href="https://beta.billbird.in"
+                target="_blank"
+                class="nav-link"
+                ><small>site</small>
+              </a>
+            </p>
+
+            <small>
+              On demand billing service built with Vuejs on frontend Django on
+              backend. Complete Business management with Billing, Customer
+              Ledger, Inventory.
+            </small>
+          </div>
+          <div class="col-6">
+            <p>
+              <span>Noter</span>
+              <a
+                href="https://github.com/soulsam480/noter"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>github</small>
+              </a>
+              <a
+                href="https://noter.sambitsahoo.com"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>site</small>
+              </a>
+            </p>
+
+            <small>
+              Noter is an open source note taking and sharing platform built
+              with Vuejs and Firebase. It has various types of text formatting
+              features with realtime note update across multiple devices.
+            </small>
+          </div>
+        </div>
+        <div class="res-data row">
+          <div class="col-6">
+            <p>
+              <span>Elec-SQLite-Vue</span>
+              <a
+                href="https://github.com/soulsam480/elec-sqlite-vue"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>github</small>
+              </a>
+            </p>
+
+            <small>
+              Proof Of Concept for Electron app with Vue 3 frontend and
+              persisted database with SQlite.
+            </small>
+          </div>
+          <div class="col-6">
+            <p>
+              <span>Shoyo</span>
+              <a
+                href="https://github.com/soulsam480/shoyo"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>github</small>
+              </a>
+              <a
+                href="https://shoyo.sambitsahoo.com"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>site</small>
+              </a>
+            </p>
+
+            <small
+              >A light and minimal CSS library. shoyo adds a very basic styling
+              for the developer to customize. It's a developer friendly library
+              in a sense where customisation is done primarily.
+            </small>
+          </div>
+        </div>
+        <div class="res-data row">
+          <div class="col-6">
+            <p>
+              <span>Ecom CMS</span>
+              <a
+                href="https://github.com/soulsam480/ecom-cms"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>github</small>
+              </a>
+              <a
+                href="https://admin.sambitsahoo.com"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>site</small>
+              </a>
+            </p>
+
+            <small>
+              Open source content management system developed in vuejs for usage
+              with firebase powered Ecommerce Web Apps.
+            </small>
+          </div>
+          <div class="col-6">
+            <p>
+              <span>Node Music</span>
+              <a
+                href="https://github.com/soulsam480/node-music"
+                target="_blank"
+                class="nav-link"
+              >
+                <small>github</small>
+              </a>
+            </p>
+
+            <small
+              >Node music is an Implementation of the FFmpeg framework to stream
+              youtube videos as mp3.
+            </small>
+          </div>
+        </div>
+        <br />
+        <a href="/projects">more projects..</a>
+      </div>
+      <div class="res-container">
+        <p class="res-head-2"><span>Education</span></p>
+        <div class="res-data">
+          <p>
+            Indira Gandhi Institute of Technology, Sarang -
+            <small class="muted"><em>B.Tech Mechanical Engineering</em></small>
+            <br />
+            <small class="muted">2017 - Present</small> <br />
+            <small>Expected Graduation June 2021, CGPA - 7.8/10 </small>
+          </p>
+          <p>
+            Naidu +2 Science College, Bhubaneswar -
+            <small class="muted"><em>XII (Senior Secondary)</em></small> <br />
+            <small class="muted">2015 - 2017</small> <br />
+            <small
+              >Council of Higher Secondary Education Odisha, Percentage-
+              73.4%</small
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+.resume {
+  h1 {
+    margin-bottom: 5px;
+  }
+  p {
+    font-size: 1em;
+  }
+  .muted {
+    color: mix(black, white, 50%);
+    a {
+      color: mix(black, white, 50%);
+      box-shadow: none;
+      transition: none;
+      border-bottom: 1px mix(black, white, 50%) dashed;
+    }
+  }
+  .res-head {
+    p {
+      margin: 4px 0;
+      a {
+        padding: 0 3px;
+      }
+    }
+  }
+  .res-head-2 {
+    span {
+      color: white;
+      padding: 0px 10px 0 5px;
+      background-color: mix(black, white, 70%);
+    }
+  }
+  .res-container {
+    padding: 10px 0;
+  }
+  .res-data {
+    padding-left: 20px;
+  }
+  .row {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+    @media (min-width: 768px) {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+  }
+  .col-6 {
+    p {
+      margin-bottom: 3px;
+      span {
+        border-bottom: 1px black dashed;
+      }
+      a {
+        padding: 0 8px;
+      }
+    }
+  }
+}
+</style>
--- content/.vitepress/theme/index.ts
+++ content/.vitepress/theme/index.ts
@@ -3,10 +3,13 @@ import '../theme/styles/code.scss';
 import { Theme } from 'vitepress';
 import Layout from './Layout.vue';
 import NotFound from './NotFound.vue';
-
+import About from './components/About.vue';
 const theme: Theme = {
   Layout,
   NotFound,
+  enhanceApp({ app }) {
+    app.component('About', About);
+  },
 };
 
 export default theme;
