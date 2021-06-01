e6c908b0be96da2b93c77516f89e4e86bb162ee5
soulsam480 <soulsam480@hotmail.com>
2021-06-01 08:55:17 +0530

    feat: added vueuse/head for rendering meta tags

--- content/.vitepress/theme/index.ts
+++ content/.vitepress/theme/index.ts
@@ -3,10 +3,15 @@ import '../theme/styles/code.scss';
 import { Theme } from 'vitepress';
 import Layout from './Layout.vue';
 import NotFound from './NotFound.vue';
+import { createHead } from '@vueuse/head';
 
 const theme: Theme = {
   Layout,
   NotFound,
+  enhanceApp: ({ app, router }) => {
+    const head = createHead();
+    app.use(head);
+  },
 };
 
 export default theme;
