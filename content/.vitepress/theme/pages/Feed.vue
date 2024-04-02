76db2a9f092408c6a351249662bbfc80674c4f41
Sambit Sahoo <soulsam480@hotmail.com>
2024-04-02 14:48:33 +0530

    feat: show a loader while giscus is loading

--- content/.vitepress/theme/pages/Feed.vue
+++ content/.vitepress/theme/pages/Feed.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
 import Giscus from "@giscus/vue";
+import { onBeforeMount, ref } from "vue";
+
+const hasLoaded = ref(false);
+
+onBeforeMount(() => {
+  window.addEventListener("message", (event) => {
+    if (event.origin !== "https://giscus.app") return;
+    if (!(typeof event.data === "object" && event.data.giscus)) return;
+
+    if (!hasLoaded.value && event.data.giscus.resizeHeight !== 0) {
+      hasLoaded.value = true;
+    }
+  });
+});
 </script>
 <template>
+  <div class="flex items-center justify-center" v-if="!hasLoaded">
+    Loading Feed....
+  </div>
+
   <Giscus
     repo="soulsam480/rengoku"
     repo-id="MDEwOlJlcG9zaXRvcnkzNjUxODYwMTY="
@@ -10,11 +28,11 @@ import Giscus from "@giscus/vue";
     mapping="number"
     strict="0"
     reactions-enabled="0"
-    emit-metadata="0"
+    emit-metadata="1"
     input-position="top"
     theme="dark_dimmed"
     lang="en"
-    loading="lazy"
+    loading="eager"
     term="14"
   />
 </template>
